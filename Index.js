9da029087b066f9d74c04f88ca2bad4affc63fe1
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-04-18 14:31:53 -0700

    Changed setup
    Changed a good portion of the code.

--- Index.js
+++ Index.js
@@ -1,31 +1,18 @@
-const UserInput = document.querySelector("#UserInput")
-const submitBtn = document.querySelector("#submitBtn");
+const imageFileInput = document.querySelector("#imageFileInput");
+const topTextInput= document.querySelector("#topTextInput");
+const bottomTextInput = document.querySelector("#bottomFileInput");
+const canvas = document.querySelector("#meme")
 
+let image;
 
-submitBtn.addEventListener("submit", () => {
-    setCookie("UserInput", UserInput.value, 365);
+imageFileInput.addEventListener("change", () => {const imageDataUrl = URL.createObjectURL(imageFileInput.files[0]);
+
+    console.log(imageDataUrl);
 });
-submitBtn.addEventListener("click", () => {
-});
 
-function setCookie(UserInput, value, daysToLive){
-    const date = new Date();
-    date.setTime(date.getTime() +  (daysToLive * 24 * 60 * 60 * 1000));
-    let expires = "expires=" + date.toUTCString();
-    document.cookie = `${UserInput}=${value}; ${expires}; path=/`
-}
-function deleteCookie(UserInput){
-    setCookie(UserInput, null, null);
-}
-function getCookie(UserInput){
-    const cDecoded = decodeURIComponent(document.cookie);
-    const cArray = cDecoded.split("; ");
-    let result = null;
-    
-    cArray.forEach(element => {
-        if(element.indexOf(UserInput) == 0){
-            result = element.substring(UserInput.length + 1)
-        }
-    })
-    return result;
+function updateMemeCanvas(canvas, image, topText, bottomtext){
+    console.log(canvas);
+    console.log(image);
+    console.log(topText);
+    console.log(bottomText);
 }
