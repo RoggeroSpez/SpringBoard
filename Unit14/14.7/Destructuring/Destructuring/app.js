fa84d931ed75a0ea6917e52a01df4007092b2d28
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-05-15 12:12:23 -0700

    Update app.js

---
+++ Unit14/14.7/Destructuring/Destructuring/app.js
@@ -0,0 +1,85 @@
+/* Object Destructuring 1*/
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); // ?
+console.log(yearNeptuneDiscovered); // ?
+
+
+/* Object Destructuring 2*/
+let planetFacts = {
+    numPlanets: 8,
+    yearNeptuneDiscovered: 1846,
+    yearMarsDiscovered: 1659
+  };
+  
+  let {numPlanets, ...discoveryYears} = planetFacts;
+  
+  console.log(discoveryYears); // ?
+
+
+/* Object Destructuring 3*/
+function getUserData({firstName, favoriteColor="green"}){
+    return `Your name is ${firstName} and you like ${favoriteColor}`;
+  }
+  
+  getUserData({firstName: "Alejandro", favoriteColor: "purple"}) // ?
+  getUserData({firstName: "Melissa"}) // ?
+  getUserData({}) // ?
+
+
+/* Arry Destructuring 1*/
+let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+console.log(first); // ?
+console.log(second); // ?
+console.log(third); // ?
+
+/* Arry Destructuring 2*/
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+    "Raindrops on roses",
+    "whiskers on kittens",
+    "Bright copper kettles",
+    "warm woolen mittens",
+    "Brown paper packages tied up with strings"
+  ]
+  
+  console.log(raindrops); // ?
+  console.log(whiskers); // ?
+  console.log(aFewOfMyFavoriteThings); // ?
+
+/* Arry Destructuring 3*/
+let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]]
+
+console.log(numbers) // ?
+
+/* ES5 Assigning Variables to Object Properties*/
+var obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+  
+  var a = obj.numbers.a;
+  var b = obj.numbers.b;
+/* ES2015 */
+
+/* ES5 Array Swap */
+var arr = [1, 2];
+var temp = arr[0];
+arr[0] = arr[1];
+arr[1] = temp;
+/* ES2015 */
+/* Race Results */
+raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre'])
+
+/*
+  {
+    first: "Tom",
+    second: "Margaret",
+    third: "Allison",
+    rest: ["David", "Pierre"]
+  }
+*/
