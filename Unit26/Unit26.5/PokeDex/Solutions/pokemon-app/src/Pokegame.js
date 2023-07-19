65b61e2401c9fcafed2148fd0a50046dd135870a
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-07-18 17:02:07 -0700

    Update Pokegame.js

--- Unit26/Unit26.5/PokeDex/Solutions/pokemon-app/src/Pokegame.js
+++ Unit26/Unit26.5/PokeDex/Solutions/pokemon-app/src/Pokegame.js
@@ -25,13 +25,13 @@ function Pokegame(props) {
 
 Pokegame.defaultProps = {
   pokemon: [
-    { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+    { id: 143, name: "Snorlax", type: "normaal", base_experience: 154 },
     { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-    { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-    { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-    { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-    { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-    { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+    { id: 267, name: "Beautifly", type: "bug/flying", base_experience: 161 },
+    { id: 12, name: "Butterfree", type: "bug/flying", base_experience: 178 },
+    { id: 82, name: "Magneton", type: "electric/steel", base_experience: 161 },
+    { id: 65, name: "Alakazam", type: "psychic", base_experience: 186 },
+    { id: 23, name: "Ekans", type: "poison", base_experience: 62 },
     { id: 133, name: "Eevee", type: "normal", base_experience: 65 }
   ]
 };
