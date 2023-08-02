0562d9eda8e305274451dc2383c965d3d7e5d2ab
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-08-02 13:45:15 -0700

    Exercise

---
+++ Unit26/Unit26.11/drawshuffle/src/Card.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+import "./Card.css";
+function Card({name, image}) {
+    //single cards get once no doubles
+    const[{ angle, xPos, yPos}] = useState({ angle: Math.random() * 90 -45, xPos: Math.random() * 40 -20, yPos: Math.random() * 40 -20,});
+     const transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+     return <img className="Card" alt={name} src={image} style={{transform}}/>;
+}
+export default Card;
+import React, { useState } from "react";
