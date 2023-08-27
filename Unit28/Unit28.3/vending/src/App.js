8e1463809192a866e3e72cd677c4b5d81a3966ad
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-08-27 13:51:46 -0700

    Update App.js

--- Unit28/Unit28.3/vending/src/App.js
+++ Unit28/Unit28.3/vending/src/App.js
@@ -3,22 +3,20 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './App.css';
 
 /*Next are imports for components*/
-/*import Navbar from"./NavBar";
 import Home from "./Home";
 import Food from "./Food";
-import Drink from "./Drink";*/
+import Drink from "./Drink";
 
 function App(){
- const [page, setPage] = useState('home');
+/* const [page, setPage] = useState('home');
  const showPage = () => {
 if (page === 'Home') return <Home/>
 if (page === 'Food') return <Food/>
 if (page === 'Drink') return <Drink/>
- }
+ }*/
  return (
   <div className="App">
     <BrowserRouter>
-    <NavBar/>
     <Routes>
 <Route path="/Home" element={<Home/>}/>
 <Route path="/Food" element={<Food/>}/>
@@ -28,6 +26,5 @@ if (page === 'Drink') return <Drink/>
 
   </div>
  );
-}
-
+ }
 export default App;
