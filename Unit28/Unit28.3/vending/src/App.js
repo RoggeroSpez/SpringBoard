02ba4d5959c244566e92f4672816e6e421841b62
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-08-27 15:08:05 -0700

    updates

--- Unit28/Unit28.3/vending/src/App.js
+++ Unit28/Unit28.3/vending/src/App.js
@@ -2,6 +2,7 @@
 import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
 import './App.css';
 /*Next are imports for components*/
+import Home from "./Home";
 import Food from "./Food";
 import Drink from "./Drink";
 import NavBar from "./NavBar";
