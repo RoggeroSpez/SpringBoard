6791531bcc4a9192aa979f03da349299f9badbb1
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-08-27 15:09:19 -0700

    Update Home.js

--- Unit28/Unit28.3/vending/src/Home.js
+++ Unit28/Unit28.3/vending/src/Home.js
@@ -2,7 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 function Home(){
 return(
-    
+    <div>
+    <BrowserRouter>
+    <NavBar/>
+    <Routes>
+<Route path="/" element={<Home/>}/>
+<Route path="/Food" element={<Food/>}/>
+<Route path="/Drink" element={<Drink/>}/>   
+    </Routes>
+    </BrowserRouter>
+  </div>
 )
 }
 
