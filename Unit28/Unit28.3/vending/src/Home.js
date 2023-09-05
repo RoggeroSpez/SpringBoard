1998865f6a5de1ac47e32a6cbd093f20d17f0811
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-09-04 17:26:05 -0700

    Update Home.js

--- Unit28/Unit28.3/vending/src/Home.js
+++ Unit28/Unit28.3/vending/src/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 /*Next are imports for components*/
-import Home from "/Home";
 import Food from "/Food";
 import Drink from "/Drink";
 import NavBar from "/NavBar";
