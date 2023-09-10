efdbaf31163900c44501556b1259413852998ed2
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-09-10 10:41:46 -0700

    Update drink.js

--- Unit28/Unit28.3/vending/src/drink.js
+++ Unit28/Unit28.3/vending/src/drink.js
@@ -1,20 +1,14 @@
-return (
+function Drink(){
+    return (
     <div>
-        <img src= />
-        <img src= />
-        <img src= />
-        <img src= />
-        <img src= />
-        <img src= />
+        <img src={} />
+        <img src={} />
+        <img src={} />
+        <img src={} />
+        <img src={} />
+        <img src={} />
       </div>
-    </div>
   );
 }
-
-
-
-
-
-
 /* Coke-Cola Sprite Fanta Gatoride Gingerale GingerBeer */
 export default Drink;
