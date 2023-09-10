95c4c1ac149cbf462c08f5691e98c29866a3e737
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-09-10 10:43:41 -0700

    Update drink.js

--- Unit28/Unit28.3/vending/src/drink.js
+++ Unit28/Unit28.3/vending/src/drink.js
@@ -1,14 +1,14 @@
 function Drink(){
     return (
     <div>
-        <img src={} />
-        <img src={} />
-        <img src={} />
-        <img src={} />
-        <img src={} />
-        <img src={} />
+        <img src={CokeCola} />
+        <img src={Sprite} />
+        <img src={Fanta} />
+        <img src={Gatorade} />
+        <img src={Gingerale} />
+        <img src={Gingerbeer} />
       </div>
   );
 }
-/* Coke-Cola Sprite Fanta Gatoride Gingerale GingerBeer */
+/* Coke-Cola Sprite Fanta Gatorade Gingerale GingerBeer */
 export default Drink;
