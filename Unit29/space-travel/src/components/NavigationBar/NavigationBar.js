a653cd8409b2af6a58e712ac0d1e1de8653b7932
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-09-18 13:38:34 -0700

    Update NavigationBar.js

--- Unit29/space-travel/src/components/NavigationBar/NavigationBar.js
+++ Unit29/space-travel/src/components/NavigationBar/NavigationBar.js
@@ -2,9 +2,18 @@ import {NavLink} from "react-router-dom";
 import styles from "./NavigationBar.module.css";
 function NavigationBar ()
 {
+  const routes = [
+    {to: "/", text:"Home"},
+    {to: "/spacecrafts", text: "Spacecrafts"},
+    {to: "/planets", text: "Planets"}
+  ];
   return (
     {
-      // todo
+      routes.map((route, index) => <NavLink key={index} className={({isActive, isPending}) =>
+      `${styles["navigation__item"]} 
+      ${isActive ? styles["navigation__item--active"] : ""}`} 
+      to={route.to}>{route.text}
+      </NavLink>)
     }
   );
 }
