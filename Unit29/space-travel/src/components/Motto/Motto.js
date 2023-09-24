cd90fbc711be5f98c8db370f7286f6e1a52442bd
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-09-24 13:17:02 -0700

    Updates

--- Unit29/space-travel/src/components/Motto/Motto.js
+++ Unit29/space-travel/src/components/Motto/Motto.js
@@ -4,7 +4,7 @@ function Motto ()
 {
   return (
     <div className={styles["motto"]}>
-      <span className={styles["motto__text"]}>In A Galaxy Far Far Away</span>
+      <span className={styles["motto__text"]}>🌌In A Galaxy Far Far Away🌌</span>
       <span className={styles["motto__emojis"]}>🥔🌎🚀🧑‍🚀🪐🥔</span>
     </div>
   );
