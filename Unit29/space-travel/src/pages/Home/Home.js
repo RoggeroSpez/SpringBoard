5560f1a6f64fcf217b1f0703a25877878a88eb69
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-09-24 12:41:41 -0700

    Update Home.js

--- Unit29/space-travel/src/pages/Home/Home.js
+++ Unit29/space-travel/src/pages/Home/Home.js
@@ -4,27 +4,27 @@ function Home ()
   return (
     <>
       <section className={styles["section__container"]}>
-        <h1>Space Travel: Expanding Horizons Beyond Earth</h1>
+        <h1>The new Frontier is ahead!🥔</h1>
       </section>
       <section className={styles["section__container"]}>
-        <h2>🌌 Journey into the Future</h2>
-        <p>In a world where the impossible has become reality, where the stars are no longer out of reach, welcome to the future of humanity's survival and exploration. Witness the evolution of technology as it transforms barren planets into thriving havens, all made possible by the wonders of innovation and human determination.</p>
+        <h2>🌌 Journey into the Unknown</h2>
+        <p>As you Travel through the stars we encourage you to be curious while being safe, the Universe is vast and full of so many Wonders</p>
       </section>
       <section className={styles["section__container"]}>
-        <h2>🌍 From Neglect to Innovation</h2>
-        <p>Once the cradle of civilization, Earth now stands as a solemn reminder of the consequences of neglect and environmental decline. But fear not, for the ingenuity of mankind has soared to new heights. With our relentless pursuit of advancement, we have not only healed our scars but extended our reach across the cosmos.</p>
+        <h2>🌍 Never Neglect to be Creative and  Innovative</h2>
+        <p>We are humans and we love to create and explore it is part of our instincts to adapt and create.</p>
       </section>
       <section className={styles["section__container"]}>
         <h2>🚀 Enter Space Travel: Where Dreams Take Flight</h2>
-        <p>Embark on an extraordinary journey with our groundbreaking web application, aptly named "Space Travel." As a commander engineer, the fate of humanity's exodus rests in your capable hands. Prepare to face the ultimate challenge: evacuating humankind from their birthplace and guiding them towards a future among the stars.</p>
+        <p>When you Embark on this extraordinary journey with our technology, we have named "Space Potatoes United Develpoment" or "S.p.u.d.", We hope that your journey is safe and a great success in the endvours of exploration in the wide cosmos.  </p>
       </section>
       <section className={styles["section__container"]}>
         <h2>🔧 Engineer, Explorer, Leader</h2>
-        <p>Space Travel empowers you to engineer, design, and even dismantle spacecraft. Craft vessels that defy the boundaries of imagination, envisioning a future where life flourishes beyond the stars. But remember, your role extends beyond construction - you are a leader, an explorer, a commander steering humanity's destiny.</p>
+        <p>Build and Create while leading the way to a better tomorrow! In the vastness of the Space adaptability is a key we of Surviving and Success in your endvours.</p>
       </section>
       <section className={styles["section__container"]}>
-        <h2>🌠 A Universe of Possibilities Awaits</h2>
-        <p>Immerse yourself in the thrill of exploration as you chart interplanetary courses within our solar system. Seamlessly navigate your fleet of spacecraft, hurtling through the cosmic void from one celestial body to another. The universe becomes your playground, and every planet a potential new home.</p>
+        <h2>🌠 A Universe of Possibilities Awaits🍟</h2>
+        <p>All ways look from multiple views so that you can see all the Possibilities that might be just out of sight.🥔</p>
       </section>
     </>
   );
