ba76a9a9258b4c42b01df7c5fb3f068a6cd974a6
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-10-08 11:33:45 -0700

    Update bubblesort.js
    Updates to the BubbleSort

--- Unit31/Unit31.2/ps_unit/bubblesort.js
+++ Unit31/Unit31.2/ps_unit/bubblesort.js
@@ -12,16 +12,21 @@ bubbleSort(nums); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67,
                   // 75, 232, 232, 453, 546, 4342]
 
 function bubbleSort(arr){
+    let count = 0;
     for(let i = 0; i < arr.length; i++){
+        let swapped = false;
         for(let j = 0; j < arr.length; j++){
+            count++;
             if(arr[j] > arr[j+1]){
                 let temp = arr [j]; 
                 arr[j] = arr[j+1]; 
                 arr[j+1] = temp;
+                swapped = true;
             }
         }
+        if(!swapped) break;
     }
+    console.log("Total Count", count)
     return arr;
-    console.log [arr];
 }
 // BubbleSort is a O(n2)(quadratic sort)//
