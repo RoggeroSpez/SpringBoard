c45ef3e819b6d9bad868c7f73da9abaf533dffb0
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-10-08 15:33:07 -0700

    Update merge.js

--- Unit31/Unit31.2/ps_unit/merge/merge.js
+++ Unit31/Unit31.2/ps_unit/merge/merge.js
@@ -111,7 +111,6 @@ function mergeSort(arr) {
     const mid = Math.floor(arr.length / 2);
     const left = mergeSort(arr.slice(0, mid));
     const right = mergeSort(arr.slice(mid));
-  
     return merge(left, right);
   }
 
