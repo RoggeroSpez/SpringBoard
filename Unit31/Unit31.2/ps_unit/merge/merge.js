1ec7257b49a7fabe381c5e68d8bc4bc33d049760
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-10-08 15:03:49 -0700

    Update merge.js

--- Unit31/Unit31.2/ps_unit/merge/merge.js
+++ Unit31/Unit31.2/ps_unit/merge/merge.js
@@ -29,7 +29,7 @@ mergeSort(nums); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35,
                  //  43, 67, 75, 232, 232, 453, 546, 4342]
 // My code begins
 
-function merge(arr1, arr2){
+function merge1(arr1, arr2){
     const r = [];
     let i = 0;
     let j = 0;
@@ -53,5 +53,53 @@ function merge(arr1, arr2){
     }
     return r;
 }
+function merge2(arr3, arr4){
+    const re = [];
+    let k = 0;
+    let l = 0;
+    while(k < arr3.length && l < arr4.length){
+        if(arr3[k] < arr4[l]){
+            re.push(arr3(k));
+            k++;
+        }
+        else {
+            re.push(arr4[l]);
+            l++;
+        }
+    }
+    while(k < arr3.length){
+        re.push(arr3[k]);
+        k++
+    }
+    while(l < arr4.length){
+        re.push(arr4[l]);
+        l++
+    }
+    return re;
+}
 
+function merge3(arr5, arr6){
+    const res = [];
+    let m = 0;
+    let n = 0;
+    while(m < arr5.length && n < arr6.length){
+        if(arr5[m] < arr6[n]){
+            res.push(arr5(m));
+            m++;
+        }
+        else {
+            res.push(arr6[n]);
+            n++;
+        }
+    }
+    while(m < arr5.length){
+        res.push(arr5[i]);
+        m++
+    }
+    while(n < arr6.length){
+        res.push(arr6[n]);
+        n++
+    }
+    return res;
+}
 // O(n log n) merging arrays use O(n+m) time/space
