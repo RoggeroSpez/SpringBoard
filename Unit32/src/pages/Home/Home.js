cd44b37791fa1fa6546bd1703df3aca2c01181c7
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-10-22 13:05:43 -0700

    add ons
    these parts of code are added from the space travel code and will be modified afterwards for the new exercise

---
+++ Unit32/src/pages/Home/Home.js
@@ -0,0 +1,32 @@
+import styles from "./Home.module.css";
+function Home ()
+{
+  return (
+    <>
+      <section className={styles["section__container"]}>
+        <h1>Welcome to Westeros</h1>
+      </section>
+      <section className={styles["section__container"]}>
+        <h2>🌌 Journey into the Unknown</h2>
+        <p>As you Travel through the stars we encourage you to be curious while being safe, the Universe is vast and full of so many Wonders</p>
+      </section>
+      <section className={styles["section__container"]}>
+        <h2>🌍 Never Neglect to be Creative and  Innovative</h2>
+        <p>We are humans and we love to create and explore it is part of our instincts to adapt and create.</p>
+      </section>
+      <section className={styles["section__container"]}>
+        <h2>🚀 Enter Space Travel: Where Dreams Take Flight</h2>
+        <p>When you Embark on this extraordinary journey with our technology, we have named "Space Potatoes United Develpoment" or "S.p.u.d.", We hope that your journey is safe and a great success in the endvours of exploration in the wide cosmos.  </p>
+      </section>
+      <section className={styles["section__container"]}>
+        <h2>🔧 Engineer, Explorer, Leader</h2>
+        <p>Build and Create while leading the way to a better tomorrow! In the vastness of the Space adaptability is a key we of Surviving and Success in your endvours.</p>
+      </section>
+      <section className={styles["section__container"]}>
+        <h2>🌠 A Universe of Possibilities Awaits🍟</h2>
+        <p>All ways look from multiple views so that you can see all the Possibilities that might be just out of sight.🥔</p>
+      </section>
+    </>
+  );
+}
+export default Home;
---
+++ Unit32/src/services/API.js
@@ -0,0 +1,19 @@
+import from"";
+class API{
+    static async getCharacter ()
+    {
+        return API.getCharacter();
+    }
+
+    static async getCharacters()
+    {
+        return API.getCharacters();
+    }
+
+    static async getHouses()
+    {
+        return API.getHouses();
+    }
+
+}
+export default API;
