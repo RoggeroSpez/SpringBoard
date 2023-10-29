ad75cc0e50c08af58dcd915ffd44b82a671377a0
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-10-28 22:40:42 -0700

    Update Characters.js

--- Unit32/src/pages/Characters/Characters.js
+++ Unit32/src/pages/Characters/Characters.js
@@ -1,16 +1,16 @@
-import {useState, useEffect, useContext} from "react";
-import { useParams } from "react-router-dom";
-import navigationBar from "./components/navigationBar";
+//import {useState, useEffect, useContext} from "react";
+//import { useParams } from "react-router-dom";
+//import navigationBar from "./components/navigationBar";
 import characters from "http://anapioficeandfire.com/api/characters/"
 
-const characters(character) {
+characters = (c) => {
 return(
     <div className="characters-container">
         <navigationBar/>
         <ol>
-        <li>"Name": "[character.name]";,</li>
-        <li>"Aliases" "[character.alias]";</li>
-        <li>"Culture": "[.culture]";</li>
+        <li>"Name": "[c.name]";,</li>
+        <li>"Aliases" "[c.alias]";</li>
+        <li>"Culture": "[c.culture]";</li>
         <li>"Gender": "gender/img";</li>
         </ol>
     </div>
