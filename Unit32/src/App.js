0f1d323d3daa6c4210754b19a9c88990707395eb
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-10-29 13:47:58 -0700

    Update App.js

--- Unit32/src/App.js
+++ Unit32/src/App.js
@@ -1,26 +1,21 @@
 import {BrowserRouter} from "react-router-dom";
-import styles from "./app.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 
 function App() {
   return (<>
-    {
       <BrowserRouter>
-      <div className={styles["app"]}>
-      <header className={styles["app__header"]}>
-      <navigationBar/>
-      </header>
-      <main className={styles["app__main"]}>
+      <div>
+      <header><navigationBar/></header>
+      <main>
         <img src="https://upload.wikimedia.org/wikipedia/en/d/dc/A_Song_of_Ice_and_Fire_book_collection_box_set_cover.jpg"
         alt="GameOfThrones A Song of Ice and Fire by George R.R. Martin" />
       </main>
-      <footer className={styles["app__footer"]}>
+      <footer>
       <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7vNNghvz2BXXcIScnjUdqf?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
       </footer>
       </div>
     </BrowserRouter>
-    }
     </>
   );
 }
