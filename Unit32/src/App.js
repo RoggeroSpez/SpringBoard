7af5a06a51c82d785d09afe55289fabbed743522
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-10-29 13:28:04 -0700

    Music
    Music media to add

--- Unit32/src/App.js
+++ Unit32/src/App.js
@@ -26,3 +26,5 @@ function App() {
 }
 
 export default App;
+
+//<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7vNNghvz2BXXcIScnjUdqf?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
