b4f127905f96ce6d3ac44f8e8ce7ae098b981b43
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-10-29 15:55:44 -0700

    Update NavigationBar.js

--- Unit32/src/components/NavigationBar/NavigationBar.js
+++ Unit32/src/components/NavigationBar/NavigationBar.js
@@ -1,19 +1,22 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import AppRoute from "../../Routes/AppRoute";
-
-function navigationBar (){
+import {NavLink} from "react-router-dom";
+import styles from "./NavigationBar.module.css";
+function NavigationBar ()
+{
+  const routes = [
+    {to: "/", text:"Home"},
+    {to: "/Houses", text: "Houses"},
+    {to: "/Characters", text: "Characters"}
+  ];
   return (
-    <div className="App">
-      <BrowserRouter>
-        <navBar />
-        <Routes>
-          <Route styles path="/" element={<home />} />
-          <Route path="/characters" element={<characters />} />
-          <Route path="/houses" element={<houses />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <nav className="navigation">
+        {
+      routes.map((route, index) => <NavLink key={index} className={({isActive, isPending}) =>
+      `${styles["navigation__item"]} 
+      ${isActive ? styles["navigation__item--active"] : ""}`} 
+      to={route.to}>{route.text}
+      </NavLink>)
+    }
+    </nav>
   );
 }
-
-export default navigationBar;
+export default NavigationBar;
