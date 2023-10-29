9018c3c68b2663109e085f49a6c914a31005687f
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-10-28 22:38:05 -0700

    Update App.js

--- Unit32/src/App.js
+++ Unit32/src/App.js
@@ -12,6 +12,8 @@ function app() {
       <navigationBar/>
       </header>
       <main className={styles["app__main"]}>
+        <img src="https://upload.wikimedia.org/wikipedia/en/d/dc/A_Song_of_Ice_and_Fire_book_collection_box_set_cover.jpg"
+        alt="GameOfThrones A Song of Ice and Fire by George R.R. Martin" />
       </main>
       <footer className={styles["app__footer"]}>
       <pagination/>
