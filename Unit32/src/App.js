90ddd37042d82a02e468699cb55ab472503d8cc2
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-10-28 19:55:51 -0700

    Updates

--- Unit32/src/App.js
+++ Unit32/src/App.js
@@ -1,13 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import styles from "./app.module.css";
+import pagination from "./components/pagination/pagination";
+import NavigationBar from "./components/NavigationBar";
+import characters from "./pages/characters";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
-import React, { useState, useEffect } from "react";
-import Pagination from "./components/Pagination/Pagination";
-import NavigationBar from "./components/NavigationBar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import characters from "./pages/characters";
 
 function app() {
-  return ();
-};
+  return (<>
+    {
+      <BrowserRouter>
+      <div className={styles["app"]}>
+      <header className={styles["app__header"]}>
+      <NavigationBar/>
+      </header>
+      <main className={styles["app__main"]}>
+      </main>
+      <footer className={styles["app__footer"]}>
+      <pagination/>
+      </footer>
+      </div>
+    </BrowserRouter>
+    }
+    </>
+  );
+}
 
 export default app;
--- Unit32/src/pages/Characters/Characters.js
+++ Unit32/src/pages/Characters/Characters.js
@@ -1,13 +1,15 @@
 import {useState, useEffect, useContext} from "react";
 import { useParams } from "react-router-dom";
+import navegationBar from "./components/nagationBar";
 import characters from "http://anapioficeandfire.com/api/characters/"
 
-function Character ()
-{
-const [selectedCharacterId, setSelectedPlanetId] = useState();
-{
-const {}
-}
-}
+const characters = () => {
+return(
+    <div className="characters-container">
+        <nagationBar/>
+        
+    </div>
+)
+};
 
 export default characters;
