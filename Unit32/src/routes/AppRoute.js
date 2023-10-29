4289cfdbf3757abeabb87f8396a183525994e3e1
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-10-29 16:53:34 -0700

    Update AppRoute.js

--- Unit32/src/routes/AppRoute.js
+++ Unit32/src/routes/AppRoute.js
@@ -1,17 +1,17 @@
 import {Routes, Route, Navigate} from "react-router-dom";
-import Home from "../pages/Home/Home";
-import House from "../pages/House";
-import Characters from "../pages/Characters";
-import Character from "../pages/Character";
+import Home from "../Pages/Home/Home";
+import House from "../Pages/House";
+import Characters from "../Pages/Characters";
+import Character from "../Pages/Character";
 
 function AppRoute ()
 {
   return (
     <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/House" element={<House/>}/>
-    <Route path="/Characters" element={<Characters/>}/>
-    <Route path="/Character/:id" element={<Character />}/>
+    <Route path="../Pages/Home/" element={<Home/>}/>
+    <Route path="../Pages/House" element={<House/>}/>
+    <Route path="../Pages/Characters" element={<Characters/>}/>
+    <Route path="../Pages/Character/:id" element={<Character />}/>
     <Route path="*" element={<Navigate to={"/"} />}/>
     </Routes>
   );
