4c0207ef6f3359f9d4b41c5f60246f974bd5e99c
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-10-29 17:13:31 -0700

    Updates

--- Unit32/src/pages/Home/Home.js
+++ Unit32/src/pages/Home/Home.js
@@ -1,18 +1,17 @@
-import styles from "./Home.module.css";
 function Home ()
 {
   return (
     <>
     <navigationBar/>
-    <section className={styles["Body"]}>
-      <section className={styles["section__container"]}>
+    <section>
+      <section>
         <h1>Welcome to the world created by George R.R. Martin</h1>
       </section>
-      <section className={styles["section__container"]}>
+      <section>
         <h2>Learn about your Favorite Game of Thrones Character</h2>
         <p>In this program you will be able to learn about the characters from the Game of thrones universe related to the box set of "A Song of Ice and Fire</p>
       </section>
-      <section className={styles["section__container"]}>
+      <section>
         <h2>Learn about the Houses</h2>
         <p>Within Westeros and Essos there are many factions from Nobility to Royalty</p>
       </section>
