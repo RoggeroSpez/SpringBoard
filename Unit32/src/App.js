a98934958c42b63c60b9d38838033625f652b595
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-10-29 19:09:12 -0700

    Update App.js

--- Unit32/src/App.js
+++ Unit32/src/App.js
@@ -14,7 +14,8 @@ function App ()
           </header>
           <main>
           <AppRoute/>
-          <img src="https://upload.wikimedia.org/wikipedia/en/d/dc/A_Song_of_Ice_and_Fire_book_collection_box_set_cover.jpg" alt="GameOfThrones A Song of Ice and Fire by George R.R. Martin" />
+          <img src="https://upload.wikimedia.org/wikipedia/en/d/dc/A_Song_of_Ice_and_Fire_book_collection_box_set_cover.jpg"  width={250} height={250}
+          alt="GameOfThrones A Song of Ice and Fire by George R.R. Martin" />
           </main>
           <footer>
           </footer>
