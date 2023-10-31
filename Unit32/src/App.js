932e5d30577a71c578e3b8192464844753ee5f61
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-10-31 11:41:57 -0700

    updates

--- Unit32/src/App.js
+++ Unit32/src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter} from "react-router-dom";
-import AppRoute from "./Routes/AppRoute";
+//import AppRoute from "./Routes/AppRoute";
 import NavBar from "./Components/navigationBar/navBar";
 
 function App ()
@@ -13,7 +13,6 @@ function App ()
             <NavBar/>
           </header>
           <main>
-          <AppRoute/>
           <img src="https://upload.wikimedia.org/wikipedia/en/d/dc/A_Song_of_Ice_and_Fire_book_collection_box_set_cover.jpg"  width={250} height={250}
           alt="GameOfThrones A Song of Ice and Fire by George R.R. Martin" />
           </main>
