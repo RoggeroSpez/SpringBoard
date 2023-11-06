d2602c0fa64c3a3fbab1b7ceb19249b709b7d294
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-11-06 14:04:19 -0800

    Update Character.js

--- Unit32/src/Pages/Character/Character.js
+++ Unit32/src/Pages/Character/Character.js
@@ -1,19 +1,28 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import NavBar from "../../Components/navigationBar/navBar";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
-const Character = () => {
+function Character() {
+  const { id } = useParams();
+  console.log('Character ID:', id);
   const [character, setCharacter] = useState(null);
   const [nextPageUrl, setNextPageUrl] = useState(null);
 
   useEffect(() => {
-    async function fetchCharacter() {
-      const response = await axios.get("https://www.anapioficeandfire.com/api/characters/1");
-      setCharacter(response.data);
-    }
+    console.log('Fetching character data for ID:', id);
+    // Make an API call to fetch character data using the 'id' parameter
+    const fetchCharacter = async () => {
+      try {
+        const response = await fetch(`https://anapioficeandfire.com/api/characters/${id}`);
+        const data = await response.json();
+        setCharacter(data);
+      } catch (error) {
+        console.error('Error fetching character data', error);
+      }
+    };
 
     fetchCharacter();
-  }, []);
+  }, [id]);
 
   const handleNextClick = () => {
     axios.get(nextPageUrl)
@@ -28,14 +37,22 @@ const Character = () => {
   
   return (
     <div>
-      <NavBar/>
-      <h1>{character.name}</h1>
-      <img src={character.portraitUrl} alt={character.name} />
-      <p>{character.aliases}</p>
-      <p>{character.culture}</p>
-      <p>{character.title}</p>
-      <p>{character.gender}</p>
-      <button onClick={handleNextClick}>Next</button>
+      {character.map (
+        <div>
+        <h1>{character.name || "Unknown"}</h1>
+          <p>House: {character.house || "Unknown"}</p>
+          <p>Born: {character.born} & Died: {character.died || "Unknown"}</p>
+          <p>Gender: {character.gender || "Unknown"}</p>
+          <p>Culture: {character.culture || "Unknown"}</p>
+          <p>Father: {character.father || "Unknown"}</p>
+          <p>Mother: {character.mother || "Unknown"}</p>
+          <p>Spouse: {character.spouse || "Unknown"}</p>
+          <p>Titles: {character.titles || "Unknown"}</p>
+          <p>Aliases: {character.aliases || "Unknown"}</p>
+          <p>Allegiance: {character.allegiances || "Unknown"}</p>
+        </div>
+          )}
+          <button onClick={handleNextClick}>Next</button>
     </div>
   );
   }
